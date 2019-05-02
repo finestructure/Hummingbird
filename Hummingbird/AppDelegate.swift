5dc7290331efe94353ad84b5dbf1bfa29726e7c4
Sven A. Schmidt <sas@finestructure.co>
2019-05-02 15:53:28 +0200

    Complete AppDelegate migration

---
+++ Hummingbird/AppDelegate.swift
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by Sven A. Schmidt on 02/05/2019.
+//  Copyright © 2019 finestructure. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var statusMenu: NSMenu!
+    var statusItem: NSStatusItem!
+    @IBOutlet weak var disabledMenuItem: NSMenuItem!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if HBSTracking.checkAXIsProcessTrusted() {
+            enable()
+        } else {
+            disabledMenuItem.state = .on
+        }
+    }
+
+    override func awakeFromNib() {
+        statusItem = HBSTracking.configure(menu: statusMenu)
+    }
+
+    func enable() {
+        disabledMenuItem.state = .off
+        HBSTracking.enable(moveResize: HBMoveResize.instance() as! HBMoveResize)
+    }
+
+    func disable() {
+        disabledMenuItem.state = .on
+        HBSTracking.disable(moveResize: HBMoveResize.instance() as! HBMoveResize)
+    }
+
+    // IBActions
+
+    @IBAction func toggleDisabled(_ sender: Any) {
+        if disabledMenuItem.state == .off {
+            disable()
+        } else {
+            enable()
+        }
+    }
+
+    @IBAction func showPreferences(_ sender: Any) {
+        HBSTracking.showPreferences(sender: sender)
+    }
+
+}
+
