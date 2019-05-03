151009073db25bfff77d508ecc5adbffb4c07e7c
Sven A. Schmidt <sas@finestructure.co>
2019-05-03 11:32:24 +0200

    Move Modifiers, State to separate files

---
+++ Hummingbird/State.swift
@@ -0,0 +1,16 @@
+//
+//  State.swift
+//  Hummingbird
+//
+//  Created by Sven A. Schmidt on 03/05/2019.
+//  Copyright © 2019 finestructure. All rights reserved.
+//
+
+import Foundation
+
+
+enum State: Int {
+    case idle
+    case moving
+    case resizing
+}
--- Hummingbird/Tracking.swift
+++ Hummingbird/Tracking.swift
@@ -167,31 +167,6 @@ func setSize(_ size: CGSize, window: AXUIElement) -> Bool {
 }
 
 
-struct Modifiers: OptionSet, Hashable {
-    let rawValue: UInt64
-
-    static let shift  = Modifiers(rawValue: CGEventFlags.maskShift.rawValue)
-    static let control  = Modifiers(rawValue: CGEventFlags.maskControl.rawValue)
-    static let alt  = Modifiers(rawValue: CGEventFlags.maskAlternate.rawValue)
-    static let command  = Modifiers(rawValue: CGEventFlags.maskCommand.rawValue)
-    static let fn  = Modifiers(rawValue: CGEventFlags.maskSecondaryFn.rawValue)
-
-    static var all: Modifiers = [.shift, .control, .alt, .command, .fn]
-
-    func exclusivelySet(in eventFlags: CGEventFlags) -> Bool {
-        return self.intersection(.all) == Modifiers(rawValue: eventFlags.rawValue).intersection(.all)
-    }
-}
-
-
-enum State: Int {
-    case idle
-    case moving
-    case resizing
-}
-
-
-
 func myCGEventCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
 
     guard let tracker = HBSTracking.tracker else {
