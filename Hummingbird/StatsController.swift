cf20287e0b5ea616eea72c1c74d4bec488b58c8a
Sven A. Schmidt <sas@finestructure.co>
2019-05-14 07:43:49 +0200

    Add StatsController with xib instead

---
+++ Hummingbird/StatsController.swift
@@ -0,0 +1,19 @@
+//
+//  StatsController.swift
+//  Hummingbird
+//
+//  Created by Sven A. Schmidt on 14/05/2019.
+//  Copyright © 2019 finestructure. All rights reserved.
+//
+
+import Cocoa
+
+class StatsController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
