1e982700284b0eee861823e264a3fc481b34058e
Sven A. Schmidt <sas@finestructure.co>
2019-08-01 12:56:36 +0200

    Add TipJar window

---
+++ Hummingbird/TipJarController.swift
@@ -0,0 +1,19 @@
+//
+//  TipJarController.swift
+//  Hummingbird
+//
+//  Created by Sven A. Schmidt on 01/08/2019.
+//  Copyright © 2019 finestructure. All rights reserved.
+//
+
+import Cocoa
+
+class TipJarController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
