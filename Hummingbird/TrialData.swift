ae42694d00eaa6317188163e84338e78a53533c5
Sven A. Schmidt <sas@finestructure.co>
2019-08-17 12:29:40 +0200

    Re-worked non-mocked version

--- Hummingbird/TrialData.swift
+++ Hummingbird/TrialData.swift
@@ -14,6 +14,7 @@ enum Status {
     case noLicenseKey
     case invalidLicenseKey
     case validLicenseKey
+    case error(Error)
 }
 
 
@@ -34,40 +35,55 @@ struct TrialData {
 }
 
 
-func validate(licenseKey: String, session: URLSession = URLSession.shared, completion: @escaping (Bool) -> ()) throws {
-    let url = URL(string: "https://api.gumroad.com/v2/licenses/verify")!
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    let body = [
-        "product_permalink": "hummingbirdapp",
-        "license_key": licenseKey,
-    ]
+struct Gumroad {
+    func validate(licenseKey: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        dataTask(licenseKey: licenseKey, completion: completion)
+    }
 
-    if let postData = try? JSONEncoder().encode(body) {
+    private func dataTask(licenseKey: String, completion: (@escaping (Result<Bool, Error>) -> Void)) {
+        var request = URLRequest(url: URL(string: "https://api.gumroad.com/v2/licenses/verify")!)
+        request.httpMethod = "POST"
+        let body = [
+            "product_permalink": "hummingbirdapp",
+            "license_key": licenseKey,
+        ]
+
+        guard let postData = try? JSONEncoder().encode(body) else {
+            completion(.failure(ValidationError.postDataEncodingError))
+            return
+        }
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = postData
-    } else {
-        throw ValidationError.postDataEncodingError
+
+        URLSession.shared.dataTask(with: request) { data, urlResponse, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                if
+                    let urlResponse = urlResponse,
+                    let httpResponse = urlResponse as? HTTPURLResponse,
+                    httpResponse.statusCode == 200 {
+                    completion(.success(true))
+                } else {
+                    completion(.success(false))
+                }
+            }
+        }.resume()
     }
 
-    let task = session.dataTask(with: request) { data, res, err in
-        if
-            let res = res,
-            let httpRes = res as? HTTPURLResponse,
-            httpRes.statusCode == 200 {
-            completion(true)
-        } else {
-            completion(false)
-        }
-    }
-    task.resume()
 }
 
 
-func validate(_ trialData: TrialData, completion: @escaping (Status) -> ()) throws {
+func validate(_ trialData: TrialData, completion: @escaping (Status) -> ()) {
     if let licenseKey = trialData.licenseKey {
-        try validate(licenseKey: licenseKey) { valid in
-            completion(valid ? .validLicenseKey : .invalidLicenseKey)
+        let api = Gumroad()
+        api.validate(licenseKey: licenseKey) { result in
+            switch result {
+            case .success(let valid):
+                completion(valid ? .validLicenseKey: .invalidLicenseKey)
+            case .failure(let error):
+                completion(.error(error))
+            }
         }
     } else {
         if trialData.inTrialPeriod {
--- HummingbirdTests/TrialTests.swift
+++ HummingbirdTests/TrialTests.swift
@@ -9,6 +9,20 @@
 import XCTest
 
 
+extension Status: Equatable {
+    static func == (lhs: Status, rhs: Status) -> Bool {
+        switch (lhs, rhs) {
+        case (.inTrial, .inTrial), (.noLicenseKey, .noLicenseKey), (.invalidLicenseKey, .invalidLicenseKey), (.validLicenseKey, .validLicenseKey):
+            return true
+        case let (.error(e1), .error(e2)):
+            return e1.localizedDescription == e2.localizedDescription
+        default:
+            return false
+        }
+    }
+}
+
+
 class TrialTests: XCTestCase {
 
     func test_noKey_inTrial() throws {
@@ -18,7 +32,7 @@ class TrialTests: XCTestCase {
             let td = TrialData(firstLaunched: firstLaunched, currentDate: currentDate, licenseKey: nil)
 
             let expectation = self.expectation(description: #function)
-            try validate(td) { status in
+            validate(td) { status in
                 XCTAssertEqual(status, .inTrial, "failed for day: \(d)")
                 expectation.fulfill()
             }
@@ -31,7 +45,7 @@ class TrialTests: XCTestCase {
         let td = TrialData(firstLaunched: firstLaunched, currentDate: day(offset: 8), licenseKey: nil)
 
         let expectation = self.expectation(description: #function)
-        try validate(td) { status in
+        validate(td) { status in
             XCTAssertEqual(status, .noLicenseKey)
             expectation.fulfill()
         }
@@ -43,22 +57,23 @@ class TrialTests: XCTestCase {
         let td = TrialData(firstLaunched: now, currentDate: now, licenseKey: "302C73F3-DB2C43BC-B2EA4E25-4C9158B4")
 
         let expectation = self.expectation(description: #function)
-        try validate(td) { status in
+        validate(td) { status in
             XCTAssertEqual(status, .validLicenseKey)
             expectation.fulfill()
         }
         waitForExpectations(timeout: 60)
     }
 
-    func test_licenseKey_invalid() throws {
+    func test_licenseKey_valid_invalid() throws {
         let now = Date()
         let td = TrialData(firstLaunched: now, currentDate: now, licenseKey: "foo")
 
         let expectation = self.expectation(description: #function)
-        try validate(td) { status in
+        validate(td) { status in
             XCTAssertEqual(status, .invalidLicenseKey)
             expectation.fulfill()
         }
         waitForExpectations(timeout: 60)
     }
+
 }
