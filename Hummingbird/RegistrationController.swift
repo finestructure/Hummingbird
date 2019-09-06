a9ef90d572ae7b9b5dee6431a2df4cfbcf4f5b95
Sven A. Schmidt <sas@finestructure.co>
2019-09-06 12:10:00 +0200

    Handling invalid keys and errors

--- Hummingbird/RegistrationController.swift
+++ Hummingbird/RegistrationController.swift
@@ -24,6 +24,7 @@ protocol RegistrationControllerDelegate: class {
 class RegistrationController: NSWindowController {
     
     @IBOutlet weak var licenseKeyField: NSTextField!
+    @IBOutlet weak var errorLabel: NSTextField!
 
     weak var delegate: RegistrationControllerDelegate?
 
@@ -39,32 +40,46 @@ class RegistrationController: NSWindowController {
         return alert
     }()
 
+
+    override func showWindow(_ sender: Any?) {
+        super.showWindow(sender)
+        errorLabel.isHidden = true
+        errorLabel.stringValue = ""
+    }
+
+
     // TODO: enabled Submit button only if string is valid
     @IBAction func submit(_ sender: Any) {
+        guard !licenseKeyField.stringValue.isEmpty else {
+            errorLabel.stringValue = "Please enter a license key."
+            errorLabel.isHidden = false
+            return
+        }
+
         let firstLaunched = Date(forKey: .firstLaunched, defaults: defaults) ?? Current.date()
         let license = License(key: licenseKeyField.stringValue)
         let licenseInfo = LicenseInfo(firstLaunched: firstLaunched, license: license)
         validate(licenseInfo) { status in
             switch status {
             case .validLicenseKey:
-                print("OK: valid license")
                 self.window?.close()
                 self.delegate?.didSubmit(license: .valid)
                 DispatchQueue.main.async {
                     self.successAlert.runModal()
                 }
-            case .inTrial:
-                print("In trial")
+            case .inTrial, .invalidLicenseKey, .noLicenseKey:
                 self.delegate?.didSubmit(license: .invalid)
-            case .noLicenseKey:
-                print("⚠️ no license")
-                self.delegate?.didSubmit(license: .invalid)
-            case .invalidLicenseKey:
-                print("⚠️ invalid license")
-                self.delegate?.didSubmit(license: .invalid)
+                DispatchQueue.main.async {
+                    self.errorLabel.stringValue = "License key invalid."
+                    self.errorLabel.isHidden = false
+                }
             case .error(let error):
                 print("⚠️ \(error)")
                 self.delegate?.didSubmit(license: .error(error))
+                DispatchQueue.main.async {
+                    self.errorLabel.stringValue = "An error occurred, please try again."
+                    self.errorLabel.isHidden = false
+                }
             }
         }
     }
