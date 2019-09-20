aaf224e0656bcb691a8ac247b82c5f6581ef0283
Sven A. Schmidt <sas@finestructure.co>
2019-09-20 15:38:55 +0200

    Linked to "I want this" by default

--- Hummingbird/Constants.swift
+++ Hummingbird/Constants.swift
@@ -19,7 +19,7 @@ enum TipSize: String {
 struct Links {
     static let accessibilityHelp = URL(string: "https://finestructure.co/hummingbird-accessibility")!
     static let gumroadLicenseVerification = URL(string: "https://api.gumroad.com/v2/licenses/verify")!
-    static let gumroadProductPage = URL(string: "https://gum.co/hummingbirdapp")!
+    static let gumroadProductPage = URL(string: "https://gum.co/hummingbirdapp?wanted=true")!
     // To: Hummingbird Help <sas+hb-help@finestructure.co, Subject: Lost license, Body: I've lost my license key 😞. Could you please send it again?
     static let retrieveLicenseKey = URL(string: "mailto:%22Hummingbird%20Help%22%3csas+hb-help@finestructure.co%3e?subject=Lost%20license%20key&body=I've%20lost%20my%20license%20key%20%F0%9F%98%9E.%20Could%20you%20please%20send%20it%20again%3F"
     )!
